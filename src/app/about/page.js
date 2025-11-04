7aa81ee6433731a7dd698b2595719aa67de83986
TopBoy7 <okefejoseph9@gmail.com>
2025-11-04 08:06:53 +0100

    aboutImages

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,5 +1,19 @@
-import React from "react";
+import Image from "next/image";
 
 export default function About() {
-  return <div></div>;
+  return (
+    <div>
+      <div className=" bg-[url('/images/about-page-bg.png')] bg-center min-h-[250px] bg-no-repeat bg-cover">
+        <div className="p-[5rem] text-white">
+          <div className="px-3">
+            <h1 className="text-3xl font-extrabold mb-3">About Us</h1>
+            <p>
+              We offer quality livestock products available for pickup or
+              delivery.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
